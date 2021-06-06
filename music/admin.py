91671594b9e97a5a4d3944b79808a92e8c5e43b9
TheBachy <60212534+TheBachy@users.noreply.github.com>
2021-06-06 14:52:47 +0200

    6.6 update, class album a attachments nefunguje

--- music/admin.py
+++ music/admin.py
@@ -4,3 +4,5 @@ from .models import *
 
 admin.site.register(Genre)
 admin.site.register(Song)
+admin.site.register(Attachment)
+admin.site.register(Album)
--- music/models.py
+++ music/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 
+def attachment_path(instance, filename):
+    return "song/" + str(instance.film.id) + "/attachments/" + filename
+
+def album_path(instance, filename):
+    return "song/" + str(instance.id) + "/album/" + filename
+
 class Genre(models.Model):
     name = models.CharField(max_length=50, unique=True, verbose_name="Genre name", help_text='Enter a song genre (e.g. rock, electro, punk...)')
 
@@ -42,12 +48,8 @@ class Song(models.Model):
 
     genres = models.ManyToManyField(Genre, help_text='Select a genre for this song')
 
-    # Metadata
 
     class Meta:
-        # Záznamy budou řazeny primárně sestupně (znaménko mínus) podle data uvedení,
-
-        # sekundárně vzestupně podle názvu
 
         ordering = ["-release_date", "title", "rate"]
 
@@ -58,3 +60,40 @@ class Song(models.Model):
     def get_absolute_url(self):
 
         return reverse('film-detail', args=[str(self.id)])
+
+
+class Album(models.Model):
+
+    title = models.CharField(max_length=200, verbose_name="Album Title")
+
+    release_date = models.DateField(blank=True, null=True,
+
+                                    help_text="Please use the following format: <em>YYYY-MM-DD</em>.",
+
+                                    verbose_name="Release date")
+
+    #songs = models.ManyToManyField(Song, help_text="select the songs that belong to this album")
+
+
+class Attachment(models.Model):
+
+    title = models.CharField(max_length=200, verbose_name="Title")
+
+    last_update = models.DateTimeField(auto_now=True)
+
+    file = models.FileField(upload_to=attachment_path, null=True, verbose_name="File")
+
+    TYPE_OF_ATTACHMENT = (
+        ('audio', 'Audio'),
+        ('image', 'Image'),
+        ('text', 'Text'),
+        ('video', 'Video'),
+        ('other', 'Other'),
+    )
+
+    type = models.CharField(max_length=5, choices=TYPE_OF_ATTACHMENT, blank=True, default='image', help_text='Select allowed attachment type', verbose_name="Attachment type")
+
+    film = models.ForeignKey(Song, on_delete=models.CASCADE)
+    # Metadata
+    class Meta:
+        ordering = ["-last_update", "type"]
